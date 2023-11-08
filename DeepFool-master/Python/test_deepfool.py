3f81304fef624186174c81dd6aa5ebd15f98f273
Jie y <1904552443@qq.com>
2023-11-08 12:30:55 +0800

    message

--- DeepFool-master/Python/test_deepfool.py
+++ DeepFool-master/Python/test_deepfool.py
@@ -11,61 +11,69 @@ from deepfool import deepfool
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-
+import tensorflow as tf
 # 创建一个 ResNet 模型，加载预训练权重
 net = models.resnet34(pretrained=True)
-
+(train_images, train_labels), (test_images, test_labels) = tf.keras.datasets.cifar10.load_data()
 # 切换到评估模式
 net.eval()
 
-# 请确保提供正确的图像文件路径
-im_orig = Image.open('output_rgb_image.jpg')
+for i in range(len(train_images)):
+    image_array = train_images[i]
+    image = Image.fromarray(image_array)
 
-mean = [0.485, 0.456, 0.406]
-std = [0.229, 0.224, 0.225]
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
 
-# 图像预处理
-im = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=mean, std=std)
-])(im_orig)
+    # 图像预处理
+    im = transforms.Compose([
+        transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])(image)
 
-r, loop_i, label_orig, label_pert, pert_image = deepfool(im, net)
+    mean = [0.485, 0.456, 0.406]
+    std = [0.229, 0.224, 0.225]
 
-# 读取标签文件
-labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
 
-mean = torch.tensor(mean, dtype=torch.float32)
-std = torch.tensor(std, dtype=torch.float32)
+    r, loop_i, label_orig, label_pert, pert_image = deepfool(im, net)
 
-str_label_orig = labels[int(label_orig)].split(',')[0]
-str_label_pert = labels[int(label_pert)].split(',')[0]
+    # 读取标签文件
+    labels = open(os.path.join('synset_words.txt'), 'r').read().split('\n')
 
-print("Original label = ", str_label_orig)
-print("Perturbed label = ", str_label_pert)
+    mean = torch.tensor(mean, dtype=torch.float32)
+    std = torch.tensor(std, dtype=torch.float32)
 
-def clip_tensor(A, minv, maxv):
-    A = torch.max(A, minv * torch.ones(A.shape))
-    A = torch.min(A, maxv * torch.ones(A.shape))
-    return A
+    str_label_orig = labels[int(label_orig)].split(',')[0]
+    str_label_pert = labels[int(label_pert)].split(',')[0]
 
-clip = lambda x: clip_tensor(x, 0, 255)
+    print("Original label = ", str_label_orig)
+    print("Perturbed label = ", str_label_pert)
 
-# 创建图像转换管道
-tf = transforms.Compose([
-    transforms.Normalize(mean=[0, 0, 0], std=list(map(lambda x: 1 / x, std))),
-    transforms.Normalize(mean=list(map(lambda x: -x, mean)), std=[1, 1, 1]),
-    transforms.Lambda(clip),
-    transforms.ToPILImage(),
-    transforms.CenterCrop(224)
-])
+    def clip_tensor(A, minv, maxv):
+        A = torch.max(A, minv * torch.ones(A.shape))
+        A = torch.min(A, maxv * torch.ones(A.shape))
+        return A
 
-plt.figure()
-# 转换pert_image为NumPy数组并显示
-pert_image_tensor = torch.from_numpy(pert_image.cpu()[0].numpy())
-plt.figure()
-plt.imshow(tf(pert_image_tensor))
-plt.title(str_label_pert)
-plt.show()
+    clip = lambda x: clip_tensor(x, 0, 255)
+
+    # 创建图像转换管道
+    tf = transforms.Compose([
+        transforms.Normalize(mean=[0, 0, 0], std=list(map(lambda x: 1 / x, std))),
+        transforms.Normalize(mean=list(map(lambda x: -x, mean)), std=[1, 1, 1]),
+        transforms.Lambda(clip),
+        transforms.ToPILImage(),
+        transforms.CenterCrop(224)
+    ])
+
+
+    figure_save_path = "bd"
+    if not os.path.exists(figure_save_path):
+        os.makedirs(figure_save_path) # 如果不存在目录figure_save_path，则创建
+
+    pert_image_tensor = torch.from_numpy(pert_image.cpu()[0].numpy())
+    plt.figure()
+    plt.imshow(tf(pert_image_tensor))
+    plt.title(str_label_pert)
+    plt.savefig(os.path.join(figure_save_path , f'{i}.jpg'))#第一个是指存储路径，第二个是图片名字
